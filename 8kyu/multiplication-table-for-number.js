d8fb0ea4980d80deaace459c5413530e5a62da68
nvjib <nvjibiman@gmail.com>
2025-02-05 14:33:49 +0300

    done

--- 8kyu/multiplication-table-for-number.js
+++ 8kyu/multiplication-table-for-number.js
@@ -4,7 +4,7 @@
 function multiTable(number) {
     let result = ""
     for (let i = 1; i <= 10; i++) {
-        result += `${i} x ${number} = ${i * number}\n`
+        result += `${i} * ${number} = ${i * number}\n`
     }
 
     return result.trim()
@@ -14,14 +14,14 @@ console.log(multiTable(5))
  
 // Output:
 /* 
-1 x 5 = 5
-2 x 5 = 10
-3 x 5 = 15
-4 x 5 = 20
-5 x 5 = 25
-6 x 5 = 30
-7 x 5 = 35
-8 x 5 = 40
-9 x 5 = 45
-10 x 5 = 50
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
 */
