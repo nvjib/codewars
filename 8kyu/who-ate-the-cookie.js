058d60066f7893306e46d72e48e55da11e6aefee
nvjib <nvjibiman@gmail.com>
2025-03-01 22:00:34 +0300

    done

---
+++ 8kyu/who-ate-the-cookie.js
@@ -0,0 +1,23 @@
+// Write a function whoAteTheCookie(input) that returns:
+
+// "Who ate the last cookie? It was Zach!" if input is a string.
+// "Who ate the last cookie? It was Monica!" if input is a number.
+// "Who ate the last cookie? It was the dog!" for anything else.
+
+// Solution
+function cookie(x){
+    if (typeof x === "string") {
+        return "Who ate the cookie? It was Zach!"
+    }
+
+    else if (typeof x === "number") {
+        return "Who ate the cookie? It was Monica!"
+    }
+
+    else {
+        return "Who ate the cookie? It was the dog!"
+    }
+  }
+
+console.log(cookie(2.3))
+
