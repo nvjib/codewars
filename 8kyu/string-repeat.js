b1505ff02ef440f89efc1c8fe10403265e50d68f
nvjib <nvjibiman@gmail.com>
2025-03-07 23:46:42 +0300

    done

---
+++ 8kyu/string-repeat.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// Solution
+const repeatStr = (n, s) => s.repeat(n) 
+console.log(repeatStr(0, "*")) 
