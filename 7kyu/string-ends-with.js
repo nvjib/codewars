5118cded9361e849723aaed64e7835483cf55bdb
nvjib <nvjibiman@gmail.com>
2025-03-14 00:06:48 +0300

    done

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,5 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string)
+
+// Solution
+const solution = (str, ending) => str.endsWith(ending)
+console.log(solution)
