2b4d3a8657c43a61e6d2672ccb26a22746e48cc0
nvjib <nvjibiman@gmail.com>
2025-03-24 11:13:19 +0300

    done

--- 7kyu/lottery-machine.js
+++ 7kyu/lottery-machine.js
@@ -1,5 +1,6 @@
 // Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
 
+// Solution
 function lottery(str){
     let onlyNumbers = str.replace(/\D/g, "")
     let uniqueNumbers = [...new Set(onlyNumbers)]
@@ -7,10 +8,9 @@ function lottery(str){
     if (!onlyNumbers) {
         return "One more run!"
     }
-    
+
     return uniqueNumbers.join("")
 }
 
- console.log(lottery("wQ8Hy0y5m5oshQPeRCkG"))
-
+ console.log(lottery("dsgfgfsgsgfdsg"))
 
