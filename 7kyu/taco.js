0cc9265e2be2fbc7eb84137d3042f282a5271a0a
nvjib <nvjibiman@gmail.com>
2025-03-28 23:51:17 +0300

    done

---
+++ 7kyu/taco.js
@@ -0,0 +1,45 @@
+// Write a function tacoTranslate(word) that takes a single string word and returns an array representing that word as a taco.
+
+// Taco Ingredient Key
+// Use the following rules to translate each letter:
+
+// Vowels (a, e, i, o, u) → "beef"
+
+// t → "tomato"
+
+// l → "lettuce"
+
+// c → "cheese"
+
+// g → "guacamole"
+
+// s → "salsa"
+
+// Ignore all other letters.
+
+// The taco must always include "shell" as the first element in the array.
+
+function tacofy(word) {    
+    const ingredients = {
+        "t": "tomato",
+        "l": "lettuce",
+        "c": "cheese",
+        "g": "guacamole",
+        "s": "salsa"
+    };
+    
+    const taco = ["shell"];
+
+    word.toLowerCase().split("").forEach((char) => {
+        if ("aeiou".includes(char)) taco.push("beef"); 
+            
+        if (ingredients[char]) taco.push(ingredients[char]); 
+    });
+
+    taco.push("shell");
+
+    return taco;
+}
+
+console.log(tacofy("ogl"))
+
