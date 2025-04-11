5bd48a874fab6e7f424f0ec86ff89cc52088f14c
nvjib <nvjibiman@gmail.com>
2025-04-11 17:29:23 +0300

    done

---
+++ 7kyu/nice-array.js
@@ -0,0 +1,5 @@
+// Write a function named isNice/IsNice that returns true if its array argument is a Nice array, else false. An empty array is not considered nice.
+
+// Solution
+const isNice = (arr) => arr.length > 0 && arr.every((num) => arr.includes(num - 1) || arr.includes(num + 1)) 
+console.log(isNice([])) 
