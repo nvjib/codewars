e1b425d5755652a349bc0da8f6b1d6bea5fccfb8
nvjib <nvjibiman@gmail.com>
2025-04-12 23:57:33 +0300

    done

--- 7kyu/nice-array.js
+++ 7kyu/nice-array.js
@@ -2,4 +2,4 @@
 
 // Solution
 const isNice = (arr) => arr.length > 0 && arr.every((num) => arr.includes(num - 1) || arr.includes(num + 1)) 
-console.log(isNice([])) 
+console.log(isNice([2, 3, 5])) 
