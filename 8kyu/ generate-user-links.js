86091a411ed713c66db3bb40c49cc7cf3c3fe6f9
nvjib <nvjibiman@gmail.com>
2025-05-15 14:22:22 +0300

    done

--- 8kyu/ generate-user-links.js
+++ 8kyu/ generate-user-links.js
@@ -1,5 +1,5 @@
 // Your task is to create userlinks for the url, you will be given a username and must return a valid link.
 
 // Solution
-const generateLink = (user) =>`http://www.codewars.com/users/${encodeURIComponent(userr)}`
+const generateLink = (user) =>`http://www.codewars.com/users/${encodeURIComponent(user)}`
 console.log(generateLink("najibiman")) 
