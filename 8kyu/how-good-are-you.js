0cc43f356e64dc43de67d3552794954d8a04ec08
nvjib <nvjibiman@gmail.com>
2025-05-19 16:16:08 +0300

    done

---
+++ 8kyu/how-good-are-you.js
@@ -0,0 +1,11 @@
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Solution
+const betterThanAverage = (clsPts, you) => clsPts.reduce((a, b) => a + b, 0) / clsPts.length < you
+console.log(betterThanAverage([2, 3], 5))
