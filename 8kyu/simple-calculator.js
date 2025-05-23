ef04b2c0ade8644e07d45c218a409855940d8676
nvjib <nvjibiman@gmail.com>
2025-05-23 11:48:44 +0300

    done

---
+++ 8kyu/simple-calculator.js
@@ -0,0 +1,26 @@
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+// if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.
+
+// Solution
+function calculator(a,b,sign){
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "unknown value"
+    }
+
+    else {
+        if (sign === "+") return a + b
+        if (sign === "-") return a - b
+        if (sign === "*") return a * b
+        if (sign === "/") return a / b
+        return "unknown value"
+    }
+    
+}
+
+console.log(calculator(1, 2, "+"))
+
