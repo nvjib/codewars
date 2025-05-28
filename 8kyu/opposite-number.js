a74ea4ce5b08e0e7f9d46971ea6abdc64bef142d
nvjib <nvjibiman@gmail.com>
2025-05-28 13:03:20 +0300

    done

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,5 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Solution
+const opposite = number => -number
+console.log(opposite(-12))
